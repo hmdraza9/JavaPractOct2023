0d9d590485bb9ae54910faf0e12d6c047bb2001e
Hamid Raza <hmdraza9@gmail.com>
2023-11-25 22:43:41 +0530

    String immutable test method

--- src/main/java/omniTopic/CheckedException.java
+++ src/main/java/omniTopic/CheckedException.java
@@ -20,7 +20,31 @@ public class CheckedException {
 
 	public static void main(String[] args) throws IOException {
 //		ch.randomMethod();
-		ch.fileAsByteToString();
+//		ch.fileAsByteToString();
+		
+		String str = "hello a";
+		String str1 = "hello a";
+		String str2 = new String("hello a");
+
+		System.out.println(str.hashCode());
+		System.out.println(str1.hashCode());
+		System.out.println(str2.hashCode());
+		
+		System.out.println();
+		
+		String s1 = "hello";
+        String s2 = new String("hello");
+        String s3 = "hello";
+        if (s1 == s2) {
+            System.out.println("s1 and s2 equal");
+        } else {
+            System.out.println("s1 and s2 not equal");
+        }
+        if (s1 == s3) {
+            System.out.println("s1 and s3 equal");
+        } else {
+            System.out.println("s1 and s3 not equal");
+        }
 
 	}
 
