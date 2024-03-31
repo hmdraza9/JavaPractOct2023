69dc7a92c8ebb5e09e4fa920bedcbd1e1c8f004a
Hamid Raza <hamid_raza@epam.com>
2024-03-31 11:16:57 +0530

    Java 8 practice

---
+++ src/main/java/test/predicates/java8/ConsumerPractice.java
@@ -0,0 +1,49 @@
+package test.predicates.java8;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class ConsumerPractice {
+
+	public static void main(String[] args) {
+
+		Consumer<Integer> cons1 = a -> System.out.print(a + "\t");
+
+		cons1.andThen(cons1).andThen(cons1).accept(11);
+		System.out.println("");
+
+		Consumer<Integer> print1to10Evens = ab -> {
+			for (int i = 0; i < ab; i++) {
+				if (i % 2 == 0) {
+					System.out.println(i);
+				}
+			}
+		};
+
+		print1to10Evens.accept(16);
+
+		System.out.println("********Modify JAVA Consumer********");
+
+		Consumer<List<Integer>> modify = list -> {
+			for (int i = 0; i < list.size(); i++) {
+				list.set(i, 2 * list.get(i));
+			}
+		};
+
+		Consumer<List<Integer>> dispList = list -> list.stream().forEach(a -> System.out.print(a + "\t"));
+
+		List<Integer> list = new ArrayList<Integer>();
+		list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+		modify.andThen(dispList).accept(list);
+		System.out.println("");
+
+		BiConsumer<Integer, String> combString = (age, name) -> System.out.println("Name: " + name + ", age: " + age);
+		
+		combString.accept(12, "New Kid");
+
+	}
+
+}
---
+++ src/main/java/test/predicates/java8/PredicatePractice.java
@@ -0,0 +1,26 @@
+package test.predicates.java8;
+
+import java.util.function.Predicate;
+
+public class PredicatePractice {
+
+	public static void main(String[] args) {
+
+		String str = "abcdef";
+		String testStr = "abc";
+
+		Predicate<String> stringContains = i -> (str.contains(i));
+
+		System.out.println(stringContains.test(testStr));
+
+		Predicate<Integer> greaterThanTen = i -> (i > 10);
+
+		Predicate<Integer> lessThanTwenty = i -> (i < 20);
+		
+		System.out.println("GreaterNLessThan: "+greaterThanTen.and(lessThanTwenty).test(19));
+		
+		System.out.println("GreaterNLessThan,negate: "+greaterThanTen.and(lessThanTwenty).negate().test(19));
+
+	}
+
+}
